18b1a3da53c30a3fb375ea8463b7c2458824b3df
Remi Collin <rcollin2@gmail.com>
2019-03-06 10:50:21 +0100

    Add MacOSX release

---
+++ build/src/K_Rush.cpp.d
@@ -0,0 +1,87 @@
+build/src/K_Rush.cpp.o: src/K_Rush.cpp src/Edge.hpp \
+  /Users/remi/Rack-SDK/include/rack.hpp \
+  /Users/remi/Rack-SDK/include/util/common.hpp \
+  /Users/remi/Rack-SDK/include/util/math.hpp \
+  /Users/remi/Rack-SDK/include/asset.hpp \
+  /Users/remi/Rack-SDK/include/plugin.hpp \
+  /Users/remi/Rack-SDK/include/tags.hpp \
+  /Users/remi/Rack-SDK/include/engine.hpp \
+  /Users/remi/Rack-SDK/dep/include/jansson.h \
+  /Users/remi/Rack-SDK/dep/include/jansson_config.h \
+  /Users/remi/Rack-SDK/include/widgets.hpp \
+  /Users/remi/Rack-SDK/dep/include/nanovg.h \
+  /Users/remi/Rack-SDK/dep/include/nanosvg.h \
+  /Users/remi/Rack-SDK/include/events.hpp \
+  /Users/remi/Rack-SDK/include/util/color.hpp \
+  /Users/remi/Rack-SDK/include/app.hpp \
+  /Users/remi/Rack-SDK/include/ui.hpp \
+  /Users/remi/Rack-SDK/dep/include/blendish.h \
+  /Users/remi/Rack-SDK/include/componentlibrary.hpp \
+  /Users/remi/Rack-SDK/include/helpers.hpp \
+  /Users/remi/Rack-SDK/include/dsp/resampler.hpp \
+  /Users/remi/Rack-SDK/dep/include/speex/speex_resampler.h \
+  /Users/remi/Rack-SDK/dep/include/speex/speexdsp_types.h \
+  /Users/remi/Rack-SDK/include/dsp/frame.hpp \
+  /Users/remi/Rack-SDK/include/dsp/ringbuffer.hpp \
+  /Users/remi/Rack-SDK/include/dsp/fir.hpp \
+  /Users/remi/Rack-SDK/include/dsp/functions.hpp \
+  /Users/remi/Rack-SDK/dep/include/pffft.h \
+  /Users/remi/Rack-SDK/include/dsp/filter.hpp
+
+src/Edge.hpp:
+
+/Users/remi/Rack-SDK/include/rack.hpp:
+
+/Users/remi/Rack-SDK/include/util/common.hpp:
+
+/Users/remi/Rack-SDK/include/util/math.hpp:
+
+/Users/remi/Rack-SDK/include/asset.hpp:
+
+/Users/remi/Rack-SDK/include/plugin.hpp:
+
+/Users/remi/Rack-SDK/include/tags.hpp:
+
+/Users/remi/Rack-SDK/include/engine.hpp:
+
+/Users/remi/Rack-SDK/dep/include/jansson.h:
+
+/Users/remi/Rack-SDK/dep/include/jansson_config.h:
+
+/Users/remi/Rack-SDK/include/widgets.hpp:
+
+/Users/remi/Rack-SDK/dep/include/nanovg.h:
+
+/Users/remi/Rack-SDK/dep/include/nanosvg.h:
+
+/Users/remi/Rack-SDK/include/events.hpp:
+
+/Users/remi/Rack-SDK/include/util/color.hpp:
+
+/Users/remi/Rack-SDK/include/app.hpp:
+
+/Users/remi/Rack-SDK/include/ui.hpp:
+
+/Users/remi/Rack-SDK/dep/include/blendish.h:
+
+/Users/remi/Rack-SDK/include/componentlibrary.hpp:
+
+/Users/remi/Rack-SDK/include/helpers.hpp:
+
+/Users/remi/Rack-SDK/include/dsp/resampler.hpp:
+
+/Users/remi/Rack-SDK/dep/include/speex/speex_resampler.h:
+
+/Users/remi/Rack-SDK/dep/include/speex/speexdsp_types.h:
+
+/Users/remi/Rack-SDK/include/dsp/frame.hpp:
+
+/Users/remi/Rack-SDK/include/dsp/ringbuffer.hpp:
+
+/Users/remi/Rack-SDK/include/dsp/fir.hpp:
+
+/Users/remi/Rack-SDK/include/dsp/functions.hpp:
+
+/Users/remi/Rack-SDK/dep/include/pffft.h:
+
+/Users/remi/Rack-SDK/include/dsp/filter.hpp:
